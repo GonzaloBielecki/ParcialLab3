1569dd8ed9351f724c57536f1e405732492c1c7c
Gonzalo Bielecki <gonzalobielecki11@gmail.com>
2024-06-28 21:48:23 -0300

    xxx

--- ProblemaReact1/src/App.jsx
+++ ProblemaReact1/src/App.jsx
@@ -3,11 +3,53 @@ import { useState } from 'react'
 import './style.css'
 
 function App() {
+  const [contraseña1,setContraseña1]=useState("")
+  const [contraseña2,setContraseña2]=useState("")
+  const [longitud,setLongitud]=useState("red")
+  const [numero,setNumero]=useState("red")
+  const [especial,setEspecial]=useState("red")
+  const [espacio,setEspacio]=useState("red")
+  const [coincidecia,setConincidencia] = useState("red")
+
+  
+  const enviar =(e)=>{
+    e.preventDefault()
+    if (contraseña1.length<8){
+      setLongitud("red")
+    }else if(contraseña1.includes("@","!","$","%","&","#")){
+      setEspecial("green")
+    }else if(!contraseña1.includes(type(Number))){
+      setNumero("red")
+    }else if(contraseña1.trim()==""){
+      setEspacio("green")
+    }
+    else{
+      setLongitud("green")
+      setEspecial("red")
+      setNumero("green")
+      
+    }
+    
+
+  }
+  
   
 
   return (
     <>
-      <p>Hola mundo</p>
+      <form onSubmit={enviar}>
+        <label htmlFor="contraseña">Ingresar Contraseña </label>
+        <input type="text" onChange={(e)=>setContraseña1(e.target.value)}/>
+        <button type='submit'>ENVIAR</button>
+       
+        <p style={{color:longitud}}>Mayor a 8 caracteres</p>
+        <p style={{color:numero}}> Incluye numero</p>
+        <p style={{color:especial}}> incluye caracter especial</p>
+        <p style={{color:espacio}}>Contiene Espacios</p>
+        <p style={{color:coincidecia}}>Las contraseña coinciden</p>
+      </form>
+
+      
     </>
   )
 }
--- ProblemaReact2/src/App.jsx
+++ ProblemaReact2/src/App.jsx
@@ -3,10 +3,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  const [persona,setPersona]=useState({
+    "nombre":"",
+    "edad":0
+  })
+  const [listaPersona,setListaPersona]=useState([""])
+
+  const agregar = (e)=>{
+    e.preventDefault()
+    setListaPersona([...listaPersona,persona])
+  }
+  console.log(listaPersona)
 
   return (
     <>
+      <form onSubmit={agregar}>
+        <label htmlFor="nombre">Ingresar Nombre</label>
+        <input type="text" onChange={(e)=>setPersona({...pesona,"nombre":e.target.value})}/>
+        <label htmlFor="edad">ingresar Edad</label>
+        <input type="number" onChange={(e)=>setPersona({...persona,"edad":e.target.value})}/>
+        <button type='submit'>Agregar</button>
+      </form>
+      
+      
       
     </>
   )
